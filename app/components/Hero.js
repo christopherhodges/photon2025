bec084e0b535a1cbf3e4d32eab799e133a500fcd
Chris Hodges <christopherhodges1986@gmail.com>
2025-06-13 13:05:34 -0400

    img height on cards

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -17,7 +17,7 @@ const Hero = ({
   return (
     <section
       className={clsx(
-        'hero relative w-full pt-[35px]',
+        'hero relative w-full',
         media
           ? 'h-[820px] lg:h-dvh lg:max-h-[822px]'
           : 'pt-[120px] lg:pt-[200px]',
