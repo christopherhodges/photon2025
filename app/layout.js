aa77a7b991f09a72a0594eec893b3a984cbad3c6
Chris Hodges <christopherhodges1986@gmail.com>
2025-06-26 18:05:14 -0400

    removed testdrive form and added google tracking code

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,7 @@ import { getNavigationMenu } from '@/lib/contentful/header';
 import { acidGrotesk } from '@/lib/fonts';
 import clsx from 'clsx';
 import Head from 'next/head';
+import Script from 'next/script';
 
 export default async function RootLayout({ children }) {
   const nav = await getNavigationMenu(false);
@@ -65,11 +66,29 @@ export default async function RootLayout({ children }) {
           type="font/woff2"
           crossOrigin="anonymous"
         />
-        {/*-- Google Tag Manager */} <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-5KGPPZ9');</script> {/* End Google Tag Manager */}
       </Head>
+      <Script
+        id="gtm-loader"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0], j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true; j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer',
+            '${process.env.NEXT_PUBLIC_GTM_ID}');`,
+        }}
+      />
       <html lang="en" className={clsx(acidGrotesk.className)}>
         <body className="text-[14px] lg:text-[16px]">
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5KGPPZ9" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5KGPPZ9"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            ></iframe>
+          </noscript>
           <BodyClass />
           <main className="l-main">
             <AnnouncementBar content="Announcement Bar" />
